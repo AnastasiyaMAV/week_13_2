d37d429d4f0423132fdb7f436db26422379f5f28
AnastasiyaMAV <mihalukova112@mail.ru>
2021-09-07 16:26:17 +0200

    Вторая правка

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
 function plus() {
     let firstNumber = Number(document.getElementById('number1').value);
     let secondNumber = Number(document.getElementById('number2').value);
-    let resultNumber = null;
     
-    resultNumber = firstNumber + secondNumber;
+    let resultNumber = firstNumber + secondNumber;
     document.getElementById('result').innerHTML = resultNumber   
 }
 
 function minus() {
     let firstNumber = Number(document.getElementById('number1').value);
     let secondNumber = Number(document.getElementById('number2').value);
-    let resultNumber = null;
     
-    resultNumber = firstNumber - secondNumber;    
+    let resultNumber = firstNumber - secondNumber;    
     document.getElementById('result').innerHTML = resultNumber;
 }
 
 function divide() {
     let firstNumber = Number(document.getElementById('number1').value);
     let secondNumber = Number(document.getElementById('number2').value);
-    let resultNumber = null;
     
-    resultNumber = firstNumber / secondNumber;    
+    let resultNumber = firstNumber / secondNumber;    
     document.getElementById('result').innerHTML = resultNumber;
 }
 
 function multiply() {
     let firstNumber = Number(document.getElementById('number1').value);
     let secondNumber = Number(document.getElementById('number2').value);
-    let resultNumber = null;
     
-    resultNumber = firstNumber * secondNumber;    
+    let resultNumber = firstNumber * secondNumber;    
     document.getElementById('result').innerHTML = resultNumber;
 }
